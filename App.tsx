00cdb18d4ead5fc6175e9cc07c39931c6bd9f687
jesse <u1110728@utah.edu>
2025-06-18 16:35:15 -0600

    --created .env and .gitignore

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { RootTabParamList } from "./scenes/NavigationTypes";
 
 import AddBusinessScene from "./scenes/AddBusinessScene/AddBusinessScene";
 import HomeScene from "./scenes/HomeScene/HomeScene";
+import { RootTabParamList } from "./scenes/NavigationTypes";
+
+
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
 export default function App() {
