e4ef6a60f10cd357ccca35fe845230e0957fa085
jesse <u1110728@utah.edu>
2026-02-20 22:35:00 -0700

    --EditBusinessSceneStyles added --EditBusinessScene now has Yup form schema

--- scenes/EditBusinessScene/EditBusinessScene.tsx
+++ scenes/EditBusinessScene/EditBusinessScene.tsx
@@ -1,16 +1,51 @@
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { TextInput } from 'react-native';
 import { useSelector } from 'react-redux';
+import * as Yup from 'yup';
 
 import getBusiness from '@/Controller/getBusiness';
 import { RootTabParamList } from '@/NavigationTypes';
 import { RootState } from '@/store/store';
 import { Business } from '@/types/Business';
+import editBusinessSceneStyles from "./EditBusinessSceneStyles";
 
 
 type props = BottomTabScreenProps<RootTabParamList, 'EditBusinessScene'>
 
+const formSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string().email("Invalid email"),
+    phone: Yup.string().matches(
+        /^(\+?\d{1,3}[-.\s]?)?(\(?\d{3}\)?[-.\s]?)?\d{3}[-.\s]?\d{4}$/,
+        'Enter valid phone number'
+    ),
+    street: Yup.string().required("Street is required"),
+    city: Yup.string().required("State is required"),
+    state: Yup.string().required("State is required"),
+    zipcode: Yup.string().required("Zipcode is required"),
+    website: Yup.string()
+        .matches(
+            /^(https?:\/\/)?([\w-]+\.)+[\w-]{2,}(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/,
+            'Enter a valid website (ex: smallmarket.com)'
+        ),
+    info: Yup.string().max(500, "info must not exceed 500 characters")
+});
+
+/**
+ * EditBusinessScene
+ * 
+ * Displays information for a single business
+ * 
+ * 
+ * @param Route -> businessId: string(MongoDb _id of the business)
+ *  
+ * Behavior: 
+ * -Fetches info of a single business
+ * -Displays info in editable fields 
+ * -Saves edited information to MongoDB.
+ * -Navigates back to ViewBusinessScene after saving. 
+ */
 const EditBusinessScene = ({ navigation, route }: props) => {
 
     const token = useSelector((state: RootState) => state.admin.token)
@@ -18,6 +53,9 @@ const EditBusinessScene = ({ navigation, route }: props) => {
 
     const [business, setBusiness] = useState<Business | null>();
 
+    /**
+     * fetch business information with id 
+     */
     useEffect(() => {
         const fetchBusiness = async () => {
             try {
@@ -29,14 +67,14 @@ const EditBusinessScene = ({ navigation, route }: props) => {
         }
 
         fetchBusiness();
-    },[]);
+    }, []);
 
 
 
     return (
-        <Text>
-            {business?.name}
-        </Text>
+        <TextInput
+            style={editBusinessSceneStyles.textInputContainer}
+        />
     )
 };
 
---
+++ scenes/EditBusinessScene/EditBusinessSceneStyles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const editBusinessSceneStyles = StyleSheet.create({
+    textInputContainer:{
+        backgroundColor: "#FFFFFF",
+    }
+});
+
+export default editBusinessSceneStyles;
